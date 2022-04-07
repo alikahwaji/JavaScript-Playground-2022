b91f9439244f1771812b214b2b7ddac7bb3ae2da
alikahwaji <ali.kahwaji@hotmail.com>
2022-04-07 16:13:45 +1200

    commiting work

--- src/app.js
+++ src/app.js
@@ -1,10 +1,15 @@
-console.log("Hello")
-console.log("Ali Kahwaji")
-console.log("Breeana")
-// TODO: 
-
-//NOTE:
-/*
-
-
-*/
+//create a function that takes an array of arrays and returns a single new array with largest numbers of each.
+// Example: findMaxNumbers([[1,2,3],[4,5,6],[7,8,9]]) -> [3,6,9]
+function findMaxnumbers(arr) {
+    let newArr = []
+    for(let i = 0; i < arr.length; i++) {
+        let maxNumber = arr[i][0]
+        for(let j = 0; j < arr[i].length; j++){
+        if(arr[i][j] > maxNumber) {
+        maxNumber = arr[i][j]
+        }
+        }
+        newArr.push(maxNumber)
+    }
+    return newArr
+}
---
+++ src/week-4.js
@@ -0,0 +1,151 @@
+//TODO
+/*
+JavaScript Variables
+*/
+// var fullName = "Ali Kahwaji"
+// var classCode = 503
+
+// console.log(fullName)
+// console.log(classCode)
+
+// JavaScript Objects
+// var full_name =
+// {
+// first_name: "Ali",
+// last_name: "Kahwaji",
+// course_number:503
+// }
+
+// console.log(full_name)
+// console.log(full_name.first_name)
+// console.log(full_name["last_name"])
+
+//JAvaScript Functions
+// function addTwoNumbers(num1, num2) {
+//  return num1+num2
+// }
+// console.log(addTwoNumbers(12,3))
+
+// function addThreeNumbers(num1, num2, num3) {
+//     return num1+num2+num3
+    
+// }
+
+// console.log(addThreeNumbers(2,5,7))
+
+// JavaScript Loops
+// for(var i=0; i< 10; i++){
+//     console.log(i)
+// }
+
+// JavaScript if statements
+// var number = 9
+// if (number === 10) {
+// console.log("answer = " + number)
+// } else {
+// console.log("error")
+// }
+
+// const and let
+// let num = 3
+// console.log(num)
+
+// num = "Ali"
+// console.log(num)
+
+// JavaScript comments
+
+/**
+Hello World
+class sdv503
+*/
+// var sdv503 = "javascript"
+
+// var x = 3
+
+// var fullName = "text"
+
+
+
+// function foo(p1, p2) {
+// return p2 - p1
+// }
+
+// foo(5, 10)
+
+// var new_value = function sum(x1, x2) {
+// return x1 + x2
+// }
+
+// sum(10, 3)
+
+// (=) vs (===)
+
+//statements vs expressions
+// var numbers
+
+// statement
+// var x
+// if (y >= 0) {
+//     x = y
+// } else {
+//     x = -y
+// }
+
+// expression ES6
+// var x = y >= 0 ? y : -y
+
+// var word = "ali"
+// console.log(word.length)
+
+// var obj = {}
+//console.log(obj)
+
+// obj.foo = 123
+// console.log(obj)
+// console.log(word.toUpperCase())
+
+// JavaScript values (primitives vs objects)
+
+// var obj1 = {}
+// var obj2 = {}
+
+// console.log(obj1 === obj1)
+
+// var number_one = 123
+// var number_two = 123
+
+// console.log(number_one === number_two)
+
+// Boolean: true and false
+//Numbers: 1234, 12432.4
+//strings: 'asdfg', "asdfg"
+//non-values: null and undefined
+
+// var str = "kahwaji"
+// console.log(str.length = 2)
+// console.log(str.length)
+// 4
+// var numbers = [1, 2, 3, 4, 5]
+// console.log(numbers[3] = 10)
+// console.log(numbers)
+
+// ({} === {})
+
+//non values
+// var nonvalue
+// console.log(nonvalue)
+
+// function nonvalue(x) {
+// return x
+// }
+
+// console.log(nonvalue())
+
+var value = function(){}
+console.log(typeof value)
+// console.log(nonvalue.first_name)
+
+//if (value === undefined || value === null)
+
+//false, 0,'', NaN
