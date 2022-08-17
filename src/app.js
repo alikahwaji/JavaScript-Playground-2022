97260e5d85f69d06ed4d49a14736a34aca7f2883
alikahwaji <ali.kahwaji@nmit.ac.nz>
2022-08-18 09:25:12 +1200

    Fixed HTML document and started the css style

--- src/app.js
+++
@@ -1,7 +0,0 @@
-<<<<<<< HEAD
-=======
-// deleteBtn.addEventListener('click', e => {
-//     let id = e.target.parentElement.getAttribute('data-id)
-//     db.colloention('todo').doc(id).delete()
-// })
->>>>>>> 864e2aac6a9e64aba3c53d6cf390573a8c59abf6
